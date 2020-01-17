a684244abeee948929c9e83c05445fb766d96647
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-16 19:45:43 -0500

    step 2 completo useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,24 @@
+import { useLocalStorage } from './useLocalStorage';
+import {useEffect} from 'react';
+import { generateTypesForGlobal } from 'typescript';
+
+const useDarkMode = (key, initialValue) => {
+    const [darkModeEnabled, setDarkModeEnabled] = useLocalStorage(key, initialValue);
+    console.log(document.body);
+
+    const toggleDarkMode = element => {
+        element.preventDefault();
+        return darkModeEnabled ? setDarkModeEnabled(false) : setDarkModeEnabled(true);
+    };
+
+    useEffect(() => {
+        if (darkModeEnabled == true) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [darkModeEnabled]);
+    return [darkModeEnabled, setDarkModeEnabled, toggleDarkMode];
+}
+
+export default useDarkMode;
